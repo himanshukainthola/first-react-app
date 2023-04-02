b5f999f2e10b40aa7630b9df88e7fde3b62c4020
Himanshu Kainthola <hkainthola6@gmail.com>
2023-04-02 18:57:14 +0530

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar () {
+    return(
+        <nav className="navbar">
+            <img src="../images/react-js-icon.svg" alt="react logo" className="navbar--logo" /> 
+            <h2 className="navbar--title">ReactFacts</h2>
+            <p className="navbar--text">React Course - Project 1</p>  
+        </nav>
+    )
+}
